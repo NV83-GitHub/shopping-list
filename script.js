2f3248cf382a3ff8601f62a5672594034bdb4375
Nicolas Vandenbruaene <nicolas.vandenbruaene@gmail.com>
2024-01-23 15:39:34 +0100

    Working mvp, still working on removing items

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-// const dbURL = process.env.DATABASE_URL
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+// const dbURL = process.env.DATABASE_URL Momentanément désactivé car incompatible avec liveserver
+
+// Firebase variables
 const appSettings = {
-    databaseURL : "ICI AJOUTE URL VERS TA DATABASE"
+    // databaseURL : "ICI AJOUTE URL VERS TA DATABASE"
+    databaseURL : "https://shoppingdb-84ee9-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
+// DOM Manipulation Variable
 const btn = document.getElementById("btn")
 const text = document.getElementById("user-input")
 const listHtml = document.getElementById("list-section")
 
-btn.addEventListener("click", function() {
+// Check la Database for existing entries then display them
+populateFromDB()
+
+// Add user input to DB and ask DB for updated Data then display them
+btn.addEventListener("click", () => {
     let inputValue = text.value
-
+    //Send new user input to DB
     push(shoppingListInDB, inputValue)
-    
-    appendItemToListHtml(inputValue)
-
-    clearInputValue()
+    //Get updated data from DB and display them
+    populateFromDB()
+    //Clean the user input field
+    clearInputValueField()
 })
 
-function clearInputValue() {
+
+function clearInputValueField() {
     text.value = ""
 }
-
-function appendItemToListHtml(item) {
-    listHtml.innerHTML += `<li>${item}</li>`
+// Wrap onValue function for cleaner code
+function populateFromDB() {
+    onValue(shoppingListInDB, function(snapshot) {
+        let shoppingItemsArray = Object.entries(snapshot.val())
+        listHtml.innerHTML = null
+        shoppingItemsArray.forEach((shopItem) => appendItemToListHtml(shopItem))
+        console.log(snapshot.val())
+    })
 }
+function appendItemToListHtml(item) {
+    listHtml.innerHTML += `<li id="${item[0]}">
+        ${item[1]}
+        <button>X</button>
+    </li>`
+}
+
